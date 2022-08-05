5d467f8ffa923f38e24c4c57788f3a47b64b7d94
B side yt SUS <100112873+567bsideyt@users.noreply.github.com>
2022-08-05 17:51:13 +0700

    Add files via upload

---
+++ assets/preload/data/babs-sneed/modchart.lua
@@ -0,0 +1,6 @@
+function onBeatHit()
+   if curBeat == 208 then
+		characterPlayAnim('dad','goback', true) --this punk sh i will never fix this i already try to fix things but the offset didn't changed at all tf
+        setProperty('defaultCamZoom',1.2)
+	end
+end
