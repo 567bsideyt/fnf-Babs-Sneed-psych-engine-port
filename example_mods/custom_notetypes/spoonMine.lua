516a900f7db6d1fccf6052340f1b451ee89751e4
B side yt SUS <100112873+567bsideyt@users.noreply.github.com>
2022-08-05 18:03:39 +0700

    Add files via upload

---
+++ example_mods/custom_notetypes/spoonMine.lua
@@ -0,0 +1,9 @@
+function onCreate()
+	for i = 0, getProperty('unspawnNotes.length')-1 do
+		if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'spoonMine' then
+			setPropertyFromGroup('unspawnNotes', i, 'noAnimation', true)
+			setPropertyFromGroup('unspawnNotes',i,'alpha',0)
+		end
+	end
+end
+		
